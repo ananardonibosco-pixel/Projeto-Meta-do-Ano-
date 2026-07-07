dd86daa17ece1d5771cd1eb2a33b3220a3cea52c
ananardonibosco-pixel <ana.nardoni.bosco@escola.pr.gov.br>
2026-07-07 07:59:25 -0300

    07/06

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 
 for (let i= 0; i < botoes.length; j++) {
     botoes[i].onclick = function ()
-    for (let = 0; j < botoes.length; j++) {
+    for (let j = 0; j < botoes.length; j++) {
         botoes[j].classList.remove("ativo"); 
     }
 
